7323d392ad19557683c31ff4f9ebc48428dc504b
1Servant96 <zhumataev.03@gmail.com>
2022-08-25 12:44:54 +0600

    changed id

--- Practise lessons/src/HousesAndPeople/Main.java
+++ Practise lessons/src/HousesAndPeople/Main.java
@@ -7,26 +7,24 @@ public class Main {
     public static void main(String[] args) {
         int v = 0;
         int b = 0;
-        Random random = new Random();
-        int a = random.nextInt(1000, 9999);
-        Person maksat = new Person(a, "Maksat", "Ylyanov", "aa@qw ", 18, "+996771098756");
-        Person asan = new Person(a, "Asan", "Asanov", "aa@Davidov", 35, "+996557431908");
-        Person aizada = new Person(a, "Aizada", "Alkanov", "as@qwerty", 21, "+996557431278");
-        Person aiperi = new Person(a, "Aiperi", "kemelkyzy", "ss@azanov.05", 32, "+996776235432");
-        Person mirbek = new Person(a, "Mirbek", "Yusupov", "qw@azanov", 77, "+996557324323");
+        Person maksat = new Person(3434, "Maksat", "Ylyanov", "aa@qw ", 18, "+996771098756");
+        Person asan = new Person(2145, "Asan", "Asanov", "aa@Davidov", 35, "+996557431908");
+        Person aizada = new Person(5234, "Aizada", "Alkanov", "as@qwerty", 21, "+996557431278");
+        Person aiperi = new Person(2353, "Aiperi", "kemelkyzy", "ss@azanov.05", 32, "+996776235432");
+        Person mirbek = new Person(1433, "Mirbek", "Yusupov", "qw@azanov", 77, "+996557324323");
 
-        Person aibek = new Person(a, "Aibek", "Atabekov", "as@mirbekatabekov.2008", 17, "+996557431278");
-        Person atabek = new Person(a, "Atabek", "Nurbekov", "@nurbekov.02", 20, "+996779308867");
-        Person almazbek = new Person(a, "Asan", "Azanov", "@azanov.95", 56, "+996999151213");
-        Person uson = new Person(a, "Uson", "Azarov", "@azarov.12", 45, "+996999121211");
-        Person davlet = new Person(a, "Davlet", "Davletuulu", "@Davletuulu", 37, "+996557431278");
+        Person aibek = new Person(3231, "Aibek", "Atabekov", "as@mirbekatabekov.2008", 17, "+996557431278");
+        Person atabek = new Person(3143, "Atabek", "Nurbekov", "@nurbekov.02", 20, "+996779308867");
+        Person almazbek = new Person(1335, "Asan", "Azanov", "@azanov.95", 56, "+996999151213");
+        Person uson = new Person(4522, "Uson", "Azarov", "@azarov.12", 45, "+996999121211");
+        Person davlet = new Person(9327, "Davlet", "Davletuulu", "@Davletuulu", 37, "+996557431278");
 
         Person[] personsOfVillageHouse = {aibek, atabek, almazbek, uson, davlet};
         Person[] personsOfBestHouse = {maksat, asan, aizada, aiperi, mirbek};
         Arrays.toString(personsOfVillageHouse);
         Arrays.toString(personsOfBestHouse);
-        VillageHouse house1 = new VillageHouse(a, "ownerJack", "Jackov", "@village.house", 50, "+996771201726", a, "villageHouse", "chuy19", 70_001, 2016, personsOfVillageHouse,personsOfVillageHouse);
-        BestHouse house2 = new BestHouse(a, "ownerSyimyk", "Syimykov", "@best.house", 37, "+996772546576", a, "bestHouse", "chuy16", 100_000, 2020, personsOfBestHouse,personsOfBestHouse);
+        VillageHouse house1 = new VillageHouse(7689, "ownerJack", "Jackov", "@village.house", 50, "+996771201726", 7685, "villageHouse", "chuy19", 70_001, 2016, personsOfVillageHouse,personsOfVillageHouse);
+        BestHouse house2 = new BestHouse(9870, "ownerSyimyk", "Syimykov", "@best.house", 37, "+996772546576", 8567, "bestHouse", "chuy16", 100_000, 2020, personsOfBestHouse,personsOfBestHouse);
 
         System.out.println("-----House in village-----");
         for (Person i: personsOfVillageHouse) {
